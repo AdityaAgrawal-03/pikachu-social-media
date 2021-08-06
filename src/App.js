e18e009dbbb387955c924e4bfda5c684fd48d03a
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-08-06 12:35:43 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> social media app </h1>
+        
+    </div>
+  );
+}
+
+export default App;
